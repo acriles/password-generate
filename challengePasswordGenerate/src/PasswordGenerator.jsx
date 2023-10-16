97353ccd00ee4ba9271011096f8f3f3ac2fc578f
diegochagas30 <140138148+diegochagas30@users.noreply.github.com>
2023-10-16 16:59:10 -0300

    Finalizando a implementação da geração de senhas

--- challengePasswordGenerate/src/PasswordGenerator.jsx
+++ challengePasswordGenerate/src/PasswordGenerator.jsx
@@ -4,6 +4,10 @@ import Slider from "./Slider";
 
 function PasswordGenerator() {
   const [password, setPassword] = useState("P4$5W0rD!");
+  const [includeLowerCase, setIncludeLowerCase]= useState(false)
+  const [includeUpperCase, setIncludeUpperCase]= useState(false)
+  const [includeNumbers, setIncludeNumbers]= useState(false)
+  const [includeSymbols, setIncludeSymbols]= useState(false)
 
   //Funções auxiliares para a geração de senhas
   function getLowerCase(){
@@ -23,12 +27,42 @@ function PasswordGenerator() {
     return symbols[Math.floor(Math.random()*symbols.length)]
   }
 
+  function setArrayFunctions(array){
+    if(includeLowerCase){
+      array.push(getLowerCase)
+    }
+    if(includeUpperCase){
+      array.push(getUpperCase)
+    }
+    if (includeNumbers) {
+      array.push(getNumber)
+    }
+    if(includeSymbols){
+      array.push(getSymbols)
+    }
+  }
+
+  //Geração de uma senha com no mínimo 4 caracteres
   function generatePassword() {
-    // Lógica para gerar uma senha
-    const newPassword = "NovaSenhaAqui"; // Substituir pela lógica de geração de senha
+    let passwordLength= Math.floor( Math.random()* 20)
+    if (passwordLength<4) {
+      passwordLength= passwordLength+ (4 - passwordLength)
+    }
+
+    let arrayFunctions= []
+    setArrayFunctions(arrayFunctions)
+
+    let new_password=""
+    let i=0
+    for(i=0;i<passwordLength;i++){
+      new_password += arrayFunctions[Math.floor( Math.random()* arrayFunctions.length)]() 
+    }
+    
+    const newPassword = new_password
     setPassword(newPassword);
   }
 
+  
   return (
     <>
       <div className="main-container">
@@ -48,13 +82,13 @@ function PasswordGenerator() {
           <Slider />
 
           <div id='checkboxes'>
-            <input type="checkbox" name="upperCaseLetters" id="upperCaseLetters" />
+            <input type="checkbox" name="upperCaseLetters" id="upperCaseLetters" checked={includeUpperCase} onChange={()=>setIncludeUpperCase(!includeUpperCase)}/>
             <label htmlFor="upperCaseLetters">Include UpperCase Letters</label>
-            <input type="checkbox" name="lowerCaseLetters" id="lowerCaseLetters" />
+            <input type="checkbox" name="lowerCaseLetters" id="lowerCaseLetters" checked={includeLowerCase} onChange={()=>setIncludeLowerCase(!includeLowerCase)}/>
             <label htmlFor="lowerCaseLetters">Include LowerCase Letters</label>
-            <input type="checkbox" name="numbers" id="numbers" />
+            <input type="checkbox" name="numbers" id="numbers" checked={includeNumbers} onChange={()=>setIncludeNumbers(!includeNumbers)}/>
             <label htmlFor="numbers">Include Numbers</label>
-            <input type="checkbox" name="symbols" id="symbols" />
+            <input type="checkbox" name="symbols" id="symbols" checked={includeSymbols} onChange={()=>setIncludeSymbols(!includeSymbols)}/>
             <label htmlFor="symbols">Include Symbols</label>
           </div>
 
@@ -71,4 +105,3 @@ function PasswordGenerator() {
 }
 
 export default PasswordGenerator
-
